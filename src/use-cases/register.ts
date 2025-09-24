628ab4488555423b37417514875265b6ec045a03
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2025-09-24 18:05:41 -0300

    Repository Pattern

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,8 +9,8 @@ interface RegisterUseCaseRequest {
 }
 
 export async function registerUseCase({
-  email,
   name,
+  email,
   password,
 }: RegisterUseCaseRequest) {
   const password_hash = await hash(password, 6)
@@ -24,11 +25,11 @@ export async function registerUseCase({
     throw new Error('Email already exist')
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }
